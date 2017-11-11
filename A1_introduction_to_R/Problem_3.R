a0d5df48d7e6856f047763c84af424aef38d5d90
Boyko Amarov <boyko.amarov@gmail.com>
2017-11-11 16:56:57 +0200

    Create Problem_3.R
    Code for Assignment 1, Problem 3.

---
+++ A1_introduction_to_R/Problem_3.R
@@ -0,0 +1,80 @@
+
+# Assignment 1 Problem 3
+
+# 1)
+
+# Install packages (needs to run only once)
+install.packages(c('dplyr', 'ggplot2'))
+
+# Load the packages
+library(dplyr)
+library(ggplot2)
+
+# 2)
+
+wines <- read.csv(file='https://s3.eu-central-1.amazonaws.com/sf-timeseries/data/wine.csv')
+str(wines)
+wines
+
+# 3)
+wines <- within(wines, {
+  Price <- exp(LogPrice)
+})
+
+# 4)
+
+wines <- within(wines, {
+  TemperatureClass <- ifelse(Temperature > mean(Temperature), 'hot', 'cold')
+})
+
+# 5)
+
+table(wines$TemperatureClass)
+
+
+# 6)
+
+winesByTempClass <- group_by(wines, TemperatureClass)
+
+summarise(winesByTempClass,
+          mean = mean(Price),
+          sd = sd(Price),
+          min = min(Price),
+          max = max(Price),
+          median = median(Price)
+          )
+# 7) Boxplot, for a very readable guide on using ggplot2 see
+## http://r4ds.had.co.nz/data-visualisation.html#introduction-1
+
+ggplot(data = wines, aes(x = TemperatureClass, y = Price)) + geom_boxplot()
+
+# 8)
+
+fit <- lm(Price ~ TemperatureClass, data = wines)
+summary(fit)
+
+# 9)
+
+ggplot(data = wines, aes(x = Temperature, y = Price)) + geom_point()
+
+# 10) (Pearson) Correlation coefficient
+
+cor(wines$Price, wines$Temperature)
+
+# 11)
+
+fit1 <- lm(Price ~ Temperature, data = wines)
+summary(fit1)
+
+# 12)
+
+ggplot(data = wines, mapping = aes(x = Temperature, y = Price)) +
+  geom_point() +
+  geom_abline(slope=0.1879, intercept = -2.8082)
+
+# or alternatively, using geom_smooth wich runs the same regression
+# from 11) to compute the coefficients
+
+ggplot(data = wines, mapping = aes(x = Temperature, y = Price)) +
+  geom_point() +
+  geom_smooth(method=lm, se=FALSE)
